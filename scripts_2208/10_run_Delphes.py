bfd31393d5cccf9661817bca6f60cf8915f419c2
Cristian Manrique <cristian.manrique@pucp.edu.pe>
2023-01-22 23:24:20 -0500

    Adding the Delphes card to the repo

--- scripts_2208/10_run_Delphes.py
+++ scripts_2208/10_run_Delphes.py
@@ -9,19 +9,19 @@ types = ['ZH', "WH", "TTH"]
 tevs = [13]
 
 for tev in tevs[:]:
-    for type in types[:]:
+    for type in types[:1]:
 
-        root = "/home/cristian/Desktop/HEP_Jones/paper_2208/scripts_2208"
-        origin = root + f"/data/bins/{tev}/{type}/"
-        destiny = root + f"/data/bins/{tev}/{type}/"
+        root = "/home/cristian/Desktop/HEP_Jones/paper_2023"
+        origin = root + f"/scripts_2208/data/bins/{tev}/{type}/"
+        destiny = root + f"/scripts_2208/data/bins/{tev}/{type}/"
 
         Path(destiny).mkdir(exist_ok=True, parents=True)
         os.system(f'rm {destiny}*.root')
 
-        for in_file in [f for f in sorted(glob.glob(origin + f'*.hepmc')) if 'pos' in f][:]:
+        for in_file in [f for f in sorted(glob.glob(origin + f'*.hepmc')) if 'pos' in f][:1]:
             #print(in_file)
             #continue
             out_file = in_file.replace('.hepmc','.root').replace(origin, destiny)
             os.system('cd /home/cristian/Programs/MG5_aMC_v2_9_2/Delphes && ./DelphesHepMC '
-                      f'cards/delphes_card_LLHNscanV4.tcl {out_file} {in_file}')
+                      f'{root}/Delphes_cards/delphes_card_LLHNscanV4.tcl {out_file} {in_file}')
             #print(zbns)
