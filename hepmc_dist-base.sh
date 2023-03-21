0dbd91846639f99f1f98d56387b6633a40266f80
Cristian Manrique <cristian.manrique@pucp.edu.pe>
2023-03-20 23:22:58 -0500

    Adding base runs to get cross sections

---
+++ hepmc_dist-base.sh
@@ -0,0 +1,27 @@
+#Script para mandar los hepmc de los runs a la carpeta correspondiente
+#!/bin/bash
+
+folder_destiny="/home/cristian/Desktop/HEP_Jones/paper_2023/scripts_2208/data/raw"
+
+tipos="ZH WH TTH"
+
+for tipo in ${tipos}
+	do
+	#declare -a arr
+	folder_origin="/home/cristian/Programs/MG5_aMC_v2_9_2/val-HN_${tipo}/Events"
+	cd ${folder_origin}
+	runs=( $(ls -d */) )
+	for run in "${runs[@]}"
+		do
+		#echo "${run}"
+		cd "${run}"
+		file_mc=("$(ls -d *_banner.txt)")
+		cross=("$(tail -9 ${file_mc} | head -1)")
+		file_mc=${file_mc%_banner.txt*}
+		file_mc=${file_mc#*("${run}")}
+		echo "${file_mc}	${cross}"
+		#file_final="$(echo "${file_mc}" | sed 's/_pythia8_events//')"
+		#cp "${file_mc}" "${folder_destiny}/run_${file_final}"	
+		cd ..
+	done
+done
---
+++ master-base.sh
@@ -0,0 +1,7 @@
+#Script para asignar un valor de coupling the Higgs a n5 n5 en todos los param cards.
+#!/bin/bash
+
+/home/cristian/Programs/MG5_aMC_v2_9_2/bin/mg5_aMC mg5_launches-base.txt
+
+bash benchs-base.sh 
+#bash hepmc_dist-base.sh 
