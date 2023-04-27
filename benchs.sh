a109394c6c765aee0cf71af896978ab84f577cd6
Cristian Manrique <cristian.manrique@pucp.edu.pe>
2023-04-26 20:08:45 -0500

    Deleting unneccesary things

--- benchs.sh
+++ benchs.sh
@@ -25,19 +25,3 @@ do
 	
 done
 
-#while IFS= read -r line
-#do
-#	width=( $line )
-# 	echo "${width[1]}"
-#  	params=( $(ls "${folder}") )
-#  	for card in "${params[@]}"
-#  	do
-#		echo "${card}"
-#		sed -i "193s/.*/DECAY  9900014   ${width[1]}/" "${folder}/${card}"
-#		mass="${card*param_card_}"    # trim from the front (left)
-#		mass="${mass%.dat*}"     trim from the back (right)
-
-#		
-#	done
-#  sed -i "193s/.*/${stringarray[1]}/" "${filename}"
-#done < "$input"
